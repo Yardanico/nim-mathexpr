40fa53a38231924202fb27268647224f13a7a397
Yardanico <21169548+Yardanico@users.noreply.github.com>
2019-09-22 22:46:39 +0300

    Add some comments, try to fix CI again.

--- src/mathexpr.nim
+++ src/mathexpr.nim
@@ -1,5 +1,5 @@
 import math, tables
-import ./mathexpr/[types, expr_eval]
+import mathexpr/[types, expr_eval]
 
 proc newEvaluator*: Evaluator =
   Evaluator(
@@ -18,24 +18,31 @@ proc addFunc*(e: Evaluator, name: string, fun: MathFunction) =
   e.funcs[name] = fun
 
 proc removeFunc*(e: Evaluator, name: string) =
+  ## Removes function with the name 'name' from the evaluator
   e.funcs.del(name)
   if e.funcs.len == 0:
     e.hasFuncs = false
 
 proc addVar*(e: Evaluator, name: string, val: float) =
+  ## Adds a constant with the name 'name' to the evaluator
   e.hasVars = true
   e.vars[name] = val
 
-proc addVars*(e: Evaluator, vars: openarray[(string, float)]) =
+template addVars*(e: Evaluator, vars: openarray[(string, float)]) =
+  ## Adds all constants from the openarray to the evaluator
   for (name, val) in vars:
     e.addVar(name, val)
 
 proc removeVar*(e: Evaluator, name: string) =
+  ## Removes the specified constant from the evaluator
   e.vars.del(name)
   if e.vars.len == 0:
     e.hasVars = false
 
 proc eval*(e: Evaluator, input: string): float =
+  ## Evaluates a math expression from 'input' and returns result as 'float'
+  ##
+  ## Can raise an exception if input  is invalid or an overflow occured
   if input.len == 0:
     raise newException(EmptyInput, "The line is empty!")
 
