cf675e59ce2f3364043c724d53569bc3694dc863
Danil Yarantsev <21169548+Yardanico@users.noreply.github.com>
2020-05-15 18:09:23 +0300

    Cosmetic changes, nimpretty

--- src/mathexpr.nim
+++ src/mathexpr.nim
@@ -1,4 +1,5 @@
-## This library is a relatively small (<500 cloc) mathematical expression evaluator library written in Nim.
+## This library is a relatively small (<500 cloc) mathematical expression 
+## evaluator library written in Nim.
 ## 
 ## The implementation is a simple recursive-descent evaluator.
 ## 
@@ -47,18 +48,20 @@
 ##   echo e.eval("work(1 2 3) + 5") # 155
 ##   echo e.eval("sqrt 100 + 5") # 15
 ## 
-## `eval` can return `NaN` or `Inf` for some inputs, such as `0/0`, or `1/0`, see `src/tests.nim` for more info
+## `eval` can return `NaN` or `Inf` for some inputs, such as `0/0`, or `1/0`, 
+## see `src/tests.nim` for more info
 
 ## What is supported?
 ## ------------------
 ## Supported operators include `+`, `-`, `/`, `*`, `%`, `^`
-## 
+##
 ## Implemented mathematical functions:
 ## * `abs(x)` - the absolute value of `x`
 ## * `acos(x)` or `arccos(x)` - the arccosine (in radians) of `x`
 ## * `asin(x)` or `arcsin(x)` - the arcsine (in radians) of `x`
 ## * `atan(x)` or `arctan(x)` or `arctg(x)` - the arctangent (in radians) of `x`
-## * `atan2(x, y)` or `arctan2(x, y)` - the arctangent of the quotient from provided `x` and `y`
+## * `atan2(x, y)` or `arctan2(x, y)` - the arctangent of the \
+## quotient from provided `x` and `y`
 ## * `ceil(x)` - the smallest integer greater than or equal to `x`
 ## * `cos(x)` - the cosine of `x`
 ## * `cosh(x)` - the hyperbolic cosine of `x`
@@ -74,8 +77,11 @@
 ## * `log2(x)` - the binary logarithm (base 2) of `x`
 ## * `max(x, y, z, ...)` - biggest argument from any number of arguments
 ## * `min(x, y, z, ...)` - smallest argument from any number of arguments
-## * `ncr(x, y)` or `binom(x, y)` - the the number of ways a sample of `y` elements can be obtained from a larger set of `x` distinguishable objects where order does not matter and repetitions are not allowed
-## * `npr(x, y)` - the number of ways of obtaining an ordered subset of `y` elements from a set of `x` elements
+## * `ncr(x, y)` or `binom(x, y)` - the the number of ways a sample of \
+## `y` elements can be obtained from a larger set of `x` distinguishable \
+## objects where order does not matter and repetitions are not allowed
+## * `npr(x, y)` - the number of ways of obtaining an ordered subset of `y` \
+## elements from a set of `x` elements
 ## * `rad(x)` - converts `x` in degrees to radians
 ## * `pow(x, y)` - the `x` to the `y` power
 ## * `sin(x)` - the sine of `x`
@@ -93,7 +99,7 @@ import math, strutils, parseutils, tables, strformat
 type
   MathFunction* = proc(args: seq[float]): float ## \
   ## Type of the procedure definition for custom math functions
-  
+
   MathCustomFun = object
     # Number of arguments this function is allowed to be called with
     argCount: int
@@ -114,16 +120,7 @@ type
     funcs: TableRef[string, MathCustomFun]
     vars: TableRef[string, float]
 
-  EmptyInput* = object of ValueError ## \
-    ## The expression was empty
-  UnbalancedParentheses* = object of ValueError ## \
-    ## Count of opening parentheses doesn't match the closing ones
-  UnexpectedCharacter* = object of ValueError ## \
-    ## Encountered an unexpected character in the input
-  UnknownIdent* = object of ValueError ## \
-    ## An unknown identifier (function or a variable)
-
-proc atEnd(expr: var MathExpression): bool = 
+proc atEnd(expr: var MathExpression): bool =
   expr.pos >= expr.len
 
 proc incPos(expr: var MathExpression, addPos = 1): char {.discardable.} =
@@ -155,23 +152,21 @@ proc eat(expr: var MathExpression, toEat: char): bool =
 proc parseExpression(expr: var MathExpression): float
 proc parseFactor(expr: var MathExpression): float
 
-proc expectedParen(expr: var MathExpression) = 
-  raise newException(UnbalancedParentheses, &"Expected ')', found '{expr.ch}'")
+proc expectedParen(expr: var MathExpression) =
+  raise newException(ValueError, &"Expected ')', found '{expr.ch}'")
 
-proc unknownIdent(funcName: string) = 
-  raise newException(UnknownIdent, &"Ident '{funcName}' is not defined")
+proc unknownIdent(funcName: string) =
+  raise newException(ValueError, &"Ident '{funcName}' is not defined")
 
-proc unexpectedChar(expr: var MathExpression) = 
-  raise newException(UnexpectedCharacter,
-    &"Unexpected character '{expr.ch}'"
-  )
+proc unexpectedChar(expr: var MathExpression) =
+  raise newException(ValueError, &"Unexpected character '{expr.ch}'")
 
 proc getArgs(expr: var MathExpression, zeroArg = true): seq[float] =
   # Gets argument list for a function call
   result = @[]
   if expr.eat('('):
     # Empty function call like a()
-    if expr.eat(')'): return 
+    if expr.eat(')'): return
     # Until it's the end of the argument list
     while expr.ch != ')':
       result.add expr.parseExpression()
@@ -185,9 +180,9 @@ proc getArgs(expr: var MathExpression, zeroArg = true): seq[float] =
     # would provide wrong results: sqrt 100 * 70
     result.add expr.parseFactor()
 
-proc checkArgLen(expected, actual: int, funcName: string) = 
+proc checkArgLen(expected, actual: int, funcName: string) =
   if expected == -1 and actual < 1:
-    raise newException(ValueError, 
+    raise newException(ValueError,
       &"Expected at least one argument for '{funcName}', got 0"
     )
   elif expected != -1 and actual != expected:
@@ -195,15 +190,14 @@ proc checkArgLen(expected, actual: int, funcName: string) =
       &"Expected {expected} arguments for '{funcName}', got {actual}"
     )
 
-template checkArgs(expected = 1) {.dirty.} = 
-  # An easier way to check if an argument call only has one argument
-  checkArgLen(expected, args.len, funcName)
+template checkArgs(actual: int, name: string, expected = 1) =
+  checkArgLen(expected, actual, name)
 
 proc parseFactor(expr: var MathExpression): float =
   # Unary + and -
   if expr.eat('+'): return expr.parseFactor()
   elif expr.eat('-'): return -expr.parseFactor()
-  
+
   if expr.eat('('):
     result = expr.parseExpression()
     if not expr.eat(')'):
@@ -221,9 +215,9 @@ proc parseFactor(expr: var MathExpression): float =
       if not data.fun.isNil():
         # Check number of arguments passed to a custom function
         let args = expr.getArgs()
-        checkArgs(data.argCount)
+        checkArgs(args.len, funcName, data.argCount)
         return data.fun(args)
-    
+
     # Built-in constants
     result = case funcName
     of "pi": PI
@@ -236,39 +230,69 @@ proc parseFactor(expr: var MathExpression): float =
     # We are *kinda* sure that we're handling a function now
     let args = expr.getArgs()
 
+    template callArgs(fnCall: untyped, argCount = 1): float =
+      # Check that the number of passed arguments is `argCount`
+      # and then does `fnCall`
+      checkArgs(args.len, funcName, argCount)
+      fnCall
+
     result = case funcName
-    of "abs": checkArgs(); abs(args[0])
-    of "acos", "arccos": checkArgs(); arccos(args[0])
-    of "asin", "arcsin": checkArgs(); arcsin(args[0])
-    of "atan", "arctan", "arctg": checkArgs(); arctan(args[0])
-    of "atan2", "arctan2": checkArgs(2); arctan2(args[0], args[1])
-    of "ceil": checkArgs(); ceil(args[0])
-    of "cos": checkArgs(); cos(args[0])
-    of "cosh": checkArgs(); cosh(args[0])
-    of "deg": checkArgs(); radToDeg(args[0])
-    of "exp": checkArgs(); exp(args[0])
-    of "sgn": checkArgs(); float sgn(args[0])
-    of "sqrt": checkArgs(); sqrt(args[0])
-    of "sum": checkArgs(-1); sum(args)
-    of "fac": checkArgs(); float fac(int(args[0]))
-    of "floor": checkArgs(); floor(args[0])
-    of "ln": checkArgs(); ln(args[0])
-    of "log", "log10": checkArgs(); log10(args[0])
-    of "log2": checkArgs(); log2(args[0])
-    of "max": checkArgs(-1); max(args)
-    of "min": checkArgs(-1); min(args)
-    of "ncr", "binom": 
-      checkArgs(2)
-      float binom(int args[0], int args[1])
-    of "npr": 
-      checkArgs(2)
-      float binom(int args[0], int args[1]) * fac(int args[1])
-    of "rad": checkArgs(); degToRad(args[0])
-    of "pow": checkArgs(2); pow(args[0], args[1])
-    of "sin": checkArgs(); sin(args[0])
-    of "sinh": checkArgs(); sinh(args[0])
-    of "tg", "tan": checkArgs(); tan(args[0])
-    of "tanh": checkArgs(); tanh(args[0])
+    of "abs":
+      callArgs abs(args[0])
+    of "acos", "arccos":
+      callArgs arccos(args[0])
+    of "asin", "arcsin":
+      callArgs arcsin(args[0])
+    of "atan", "arctan", "arctg":
+      callArgs arctan(args[0])
+    of "atan2", "arctan2":
+      callArgs arctan2(args[0], args[1]), 2
+    of "ceil":
+      callArgs ceil(args[0])
+    of "cos":
+      callArgs cos(args[0])
+    of "cosh":
+      callArgs cosh(args[0])
+    of "deg":
+      callArgs radToDeg(args[0])
+    of "exp":
+      callArgs exp(args[0])
+    of "sgn":
+      callArgs float(sgn(args[0]))
+    of "sqrt":
+      callArgs sqrt(args[0])
+    of "sum":
+      callArgs sum(args), -1
+    of "fac":
+      callArgs float(fac(int(args[0])))
+    of "floor":
+      callArgs floor(args[0])
+    of "ln":
+      callArgs ln(args[0])
+    of "log", "log10":
+      callArgs log10(args[0])
+    of "log2":
+      callArgs log2(args[0])
+    of "max":
+      callArgs max(args), -1
+    of "min":
+      callArgs min(args), -1
+    of "ncr", "binom":
+      callArgs float(binom(int args[0], int args[1])), 2
+    of "npr":
+      callArgs float(binom(int args[0], int args[1]) * fac(int args[1])), 2
+    of "rad":
+      callArgs degToRad(args[0])
+    of "pow":
+      callArgs pow(args[0], args[1]), 2
+    of "sin":
+      callArgs sin(args[0])
+    of "sinh":
+      callArgs sinh(args[0])
+    of "tg", "tan":
+      callArgs tan(args[0])
+    of "tanh":
+      callArgs tanh(args[0])
     else:
       unknownIdent(funcname)
       NaN
@@ -276,7 +300,7 @@ proc parseFactor(expr: var MathExpression): float =
   # Numbers ('.' is for numbers like '.5')
   elif expr.ch in {'0'..'9', '.'}:
     expr.incPos(parseFloat(expr.input, result, expr.pos))
-  
+
   else:
     expr.unexpectedChar()
 
@@ -298,7 +322,7 @@ proc parseExpression(expr: var MathExpression): float =
     of '-': result -= expr.parseTerm()
     else: break
 
-proc parse(expr: var MathExpression): float = 
+proc parse(expr: var MathExpression): float =
   result = expr.parseExpression()
   # We should parse the whole input, otherwise we skipped something
   # and that's certainly not good
@@ -306,9 +330,9 @@ proc parse(expr: var MathExpression): float =
 
 proc newEvaluator*: Evaluator =
   ## Creates a new evaluator instance for evaluating math expressions
-  ## 
-  ## 
-  ## There's no limit on the number of evaluator instances, 
+  ##
+  ##
+  ## There's no limit on the number of evaluator instances,
   ## and all functions and math procedures are local to the current instance
   Evaluator(
     vars: newTable[string, float](),
@@ -320,10 +344,10 @@ proc newEvaluator*: Evaluator =
 proc addFunc*(e: Evaluator, name: string, fun: MathFunction, argCount = -1) =
   ## Adds custom function `fun` with the name `name` to the evaluator `e`
   ## which will then be available inside of all following `e.eval()` calls.
-  ## 
+  ##
   ## `argCount` specifies the number of arguments this function is allowed
-  ## to be called with. If it is `-1`, function will work with any
-  ## number of arguments
+  ## to be called with. If it is `-1`, the function will be able to be called
+  ## with one or more arguments, otherwise - only with `argCount` arguments.
   e.hasFuncs = true
   e.funcs[name] = MathCustomFun(fun: fun, argCount: argCount)
 
@@ -344,7 +368,7 @@ proc addVars*(e: Evaluator, vars: openArray[(string, float)]) =
     let e = newEvaluator()
     e.addVars({"a": 3.0, "b": 5.5})
     assert e.eval("a ^ a + b") == 32.5
-  
+
   for (name, val) in vars:
     e.addVar(name, val)
 
@@ -354,18 +378,18 @@ proc removeVar*(e: Evaluator, name: string) =
   if e.vars.len == 0:
     e.hasVars = false
 
-proc eval*(e: Evaluator, input: sink string): float =
+proc eval*(e: Evaluator, input: string): float =
   ## Evaluates a math expression from `input` and returns result as `float`
   ##
   ## Can raise an exception if `input` is invalid or an overflow occured
   if input.len == 0:
-    raise newException(EmptyInput, "The line is empty!")
+    raise newException(ValueError, "The line is empty!")
 
   var expr = MathExpression(
     eval: e,
     len: input.len,
     ch: input[0],
-    input: move(input),
+    input: input,
     pos: 0
   )
 
